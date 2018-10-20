f389973a267f23c2026eee38e8bfbafd293df285
sekayasin <sekayasin@gmail.com>
2018-10-20 11:16:48 +0300

    ch-testing-api-160981387
    no tests
    
    [Delivers #160981387]

--- tests/test_api.py
+++ tests/test_api.py
@@ -4,9 +4,9 @@ from flask import Response, json
 from api import fff, routes, status
 
 
-def test_index():
-    client = fff.test_client()
-    res = client.get('/')
-    assert res.status_code == status.HTTP_302_FOUND
+# def test_index():
+#     client = fff.test_client()
+#     res = client.get('/')
+#     assert res.status_code == status.HTTP_302_FOUND
 
     
